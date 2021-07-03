4eca6b40f122d59da6d36e9bc3c54825c65dbcb2
CATHEALPE <cathealpe@gmail.com>
2021-07-02 19:56:01 -0500

    eliminacion linea11

--- src/App.java
+++ src/App.java
@@ -8,6 +8,6 @@ class Programa {
     public static void saludo(String nombre){ //se puede hacer funciones con el mismo nombre en la misma clase, pero deben recibir diferentes parametros
         //como la salida va a ser un string se le quitan los corchetes, los corchetes son para un arreglo de strings
         System.out.print("Hola " + nombre + "!");
-        System.out.print("Hola " + nombre + "!");
+
     }
 }
